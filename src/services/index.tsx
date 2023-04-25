d5d2f4d6fcdfd2b75b1f4909ef8066a2788ac7c4
lihongliang <lihongliang@datagrand.com>
2023-04-25 17:14:47 +0800

    fix: update

--- src/services/index.tsx
+++ src/services/index.tsx
@@ -4,12 +4,15 @@ import { get, pick } from 'lodash-es';
 
 class ChatService {
   async genWeekly(module: IModule, text: string, params, session_id: string) {
-    const res = await $http.post<Promise<ISafeAny>>('api/generate', {
-      module,
-      text,
-      params,
-      session_id,
-    });
+    const res = await $http.post<Promise<ISafeAny>>(
+      'http://47.103.101.101/api/generate',
+      {
+        module,
+        text,
+        params,
+        session_id,
+      }
+    );
     return res.data;
   }
 }
